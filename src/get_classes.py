27727aef8fbe66951b2b7c9327ad6418a8d55da7
SamanthaLLee <samantha.lin.lee@gmail.com>
2022-04-02 15:27:20 -0400

    fetched all courses

--- src/get_classes.py
+++ src/get_classes.py
@@ -1,7 +1,36 @@
 import requests
+import json
+endpoint = "http://sis.rutgers.edu/oldsoc/courses.json?semester=12022&campus=NB&level=UG&subject="
+codemap = dict()
+dptcodes = []
 
 
-endpoint = "https://sis.rutgers.edu/oldsoc/courses.json?semester=12022&campus=NB&level=UG?coreCode=CC&subject="
-response = requests.get("http://api.open-notify.org/astros.json")
+def organize_codes():
+    f = open("src/dpt_codes.txt", "r")
+    lines = f.readlines()
+    for line in lines:
+        code = line[0:3]
+        dptcodes.append(str(code))
 
-print(response)
+
+def get_requests():
+    first = True
+    with open('all_responses.json', 'w') as f:
+        f.write("[")
+        for i, code in enumerate(dptcodes):
+            response = requests.get(endpoint+str(code))
+            if len(response.content) > 2:
+                if first:
+                    first = False
+                else:
+                    f.write(",")
+                string = str(response.content.decode(
+                    encoding="utf8", errors='ignore'))
+                string = string[1:-1]
+                f.write(string)
+        f.write("]")
+
+
+if __name__ == "__main__":
+    organize_codes()
+    get_requests()
