e756487a225ae2cdc27e3f455a482dcc0a18fe7c
Matthew Notaro <matthew.y.notaro@gmail.com>
2022-04-02 19:47:36 -0400

    cleaned up parsing

--- src/professor.py
+++ src/professor.py
@@ -1,28 +1,22 @@
 import ratemyprofessor
 
-class Professor:
-    def __init__(self) -> None:
-        self.prof_name = None
-        self.prof_rating = None
-        self.prof_diff = None
-        self.prof_department = None
-    
-    def get_RMP(self):
-        # TODO: can make this a constant somewhere
-        correct_rutgers = ratemyprofessor.get_schools_by_name("Rutgers state")[1]
+# School ID
+correct_rutgers = ratemyprofessor.get_schools_by_name("Rutgers state")[1]
 
-        professor = ratemyprofessor.get_professors_by_school_and_name(correct_rutgers, self.prof_name)
+class Professor:
+    def __init__(self, name) -> None:
+        self.name = name
+        self.rating = None # / 5
+        self.diff = None # / 5
+        self.dept = None
+
+        self.get_RMP()
+    
+    # Finds best matching professor from RMP and populates rating, difficulty, and department
+    def get_RMP(self):
+        professor = ratemyprofessor.get_professors_by_school_and_name(correct_rutgers, self.name)
 
         if professor is not None:
-            # print("%s works in the %s Department of %s." % (professor.name, professor.department, professor.school.name))
-            # print("Rating: %s / 5.0" % professor.rating)
-            # print("Difficulty: %s / 5.0" % professor.difficulty)
-            # print("Total Ratings: %s" % professor.num_ratings)
-            # if professor.would_take_again is not None:
-            #     print(("Would Take Again: %s" % round(professor.would_take_again, 1)) + '%')
-            # else:
-            #     print("Would Take Again: N/A")
-            
-            self.prof_diff = professor.difficulty
-            self.prof_rating = professor.rating
-            self.prof_department = professor.department
+            self.diff = professor.difficulty
+            self.rating = professor.rating
+            self.dept = professor.department
