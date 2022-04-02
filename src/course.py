2bff57dbdb294ecf537eb318c4a0f04d3f664411
Matthew Notaro <matthew.y.notaro@gmail.com>
2022-04-02 18:51:24 -0400

    soc

--- src/course.py
+++ src/course.py
@@ -1,7 +1,7 @@
 import ratemyprofessor
 
 class Course:
-    def __init__(self, name) -> None:
+    def __init__(self, name, code, core) -> None:
         self.name = name
         self.code = None
         self.core = None
